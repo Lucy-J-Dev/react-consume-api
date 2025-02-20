5d8e5689ab12a891629e67adc82e4cb536e621bd
Lucy Jiménez <lucy.jimenez.mejia@gmail.com>
2025-02-20 16:14:08 -0500

    Agregar validaciones y mensajes de error y exito al formulario

--- src/components/FormExample.jsx
+++ src/components/FormExample.jsx
@@ -6,20 +6,52 @@ const initialValue = {
   email: "",
 };
 
+const initialErrors = {};
+
 const FormExample = () => {
   const [formData, setFormData] = useState(initialValue);
+  const [formErrors, setFormErrors] = useState(initialErrors);
+  const [successfulMessage, setSuccessfulMessage] = useState("");
 
   const handleInputChange = (event) => {
-    const propName = event.target.name;
-    setFormData({ ...formData, [propName]: event.target.value });
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
   };
 
   const handleForm = (event) => {
     event.preventDefault();
+
+    setSuccessfulMessage("");
+
+    // Validar el formulario
+    const errors = {};
+
+    if (formData.name.trim() === "") {
+      errors.name = "No hay un nombre";
+    }
+
+    if (formData.age < 18) {
+      errors.age = "La edad no corresponde";
+    }
+
+    if (formData.email.trim() === "") {
+      errors.email = "No hay un correo electronico";
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      errors.email = "El correo no es valido";
+    }
+
+    setFormErrors(errors);
+
     console.log(
       `Nombre: ${formData.name} - Edad: ${formData.age} - Correo: ${formData.email}`
     );
-    setFormData(initialValue);
+
+    const existenErrores = Object.keys(errors).length > 0;
+
+    if (!existenErrores) {
+      setSuccessfulMessage("Su formulario fue completado correctamente");
+      setFormData(initialValue);
+    }
   };
 
   return (
@@ -34,8 +66,17 @@ const FormExample = () => {
             name="name"
             id="name"
             value={formData.name}
-            onChange={handleInputChange}
+            onChange={(event) => {
+              const valor = event.target.value;
+              setFormData({ ...formData, name: valor });
+              console.log(formData.name);
+            }}
           />
+          {formErrors.name && (
+            <div>
+              <small style={{ color: "red" }}>{formErrors.name}</small>
+            </div>
+          )}
         </div>
         <div>
           <label htmlFor="age">Edad</label>
@@ -46,6 +87,11 @@ const FormExample = () => {
             value={formData.age}
             onChange={handleInputChange}
           />
+          {formErrors.age && (
+            <div>
+              <small style={{ color: "red" }}>{formErrors.age}</small>
+            </div>
+          )}
         </div>
         <div>
           <label htmlFor="email">Correo electronico</label>
@@ -56,8 +102,18 @@ const FormExample = () => {
             value={formData.email}
             onChange={handleInputChange}
           />
+          {formErrors.email && (
+            <div>
+              <small style={{ color: "red" }}>{formErrors.email}</small>
+            </div>
+          )}
         </div>
         <button type="submit">Registrar</button>
+        {successfulMessage && (
+          <div>
+            <p style={{ color: "green" }}>{successfulMessage}</p>
+          </div>
+        )}
       </form>
     </>
   );
